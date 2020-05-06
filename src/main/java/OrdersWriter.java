57b99f0640a01c95d901b5d02d73f03507265442
Maurice <m.lehmann94@gmail.com>
2020-05-06 11:53:56 +0200

    refactor display of a order & orders. The class order and orders can now print itself , #badsmell extract display mecanics

--- src/main/java/OrdersWriter.java
+++ src/main/java/OrdersWriter.java
@@ -9,31 +9,7 @@ public class OrdersWriter {
 
     public String getContents() {
         StringBuffer sb = new StringBuffer("{\"orders\": [");
-
-        for (int i = 0; i < orders.getOrdersCount(); i++) {
-            Order order = orders.getOrder(i);
-            sb.append("{");
-            sb.append("\"id\": ");
-            sb.append(order.getOrderId());
-            sb.append(", ");
-            sb.append("\"products\": [");
-            for (int j = 0; j < order.getProductsCount(); j++) {
-                Product product = order.getProduct(j);
-                sb.append(product.toString());
-                sb.append(", ");
-            }
-            if (order.getProductsCount() > 0) {
-                sb.delete(sb.length() - 2, sb.length());
-            }
-
-            sb.append("]");
-            sb.append("}, ");
-        }
-
-        if (orders.getOrdersCount() > 0) {
-            sb.delete(sb.length() - 2, sb.length());
-        }
-
+        sb.append(orders.toString());
         return sb.append("]}").toString();
     }
 
